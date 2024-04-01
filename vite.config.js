2fb77452e5b4ebaaf74900838c1aa4d5c22190ff
Swagatika <mswagatika3009@gmail.com>
2024-04-01 13:10:01 +0530

    vite congig change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pomodor__Timer/",
+  base: "/pomodoro_timer/",
   plugins: [react()],
 });
